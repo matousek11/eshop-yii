2ea0494c5687e449924e62770173f7eb5ee88f65
matousek11 <matousek1199@gmail.com>
2023-08-27 21:12:17 +0200

    Link removed

--- widgets/Item/views/item-view.php
+++ widgets/Item/views/item-view.php
@@ -1,5 +1,5 @@
 <div class="main-container col-lg-2 py-3">
-    <a class="text-decoration-none text-dark" href=<?= "/" . $id ?>>
+    <a class="text-decoration-none text-dark" href="/">
         <div class="py-5 px-2">
             <img class="w-100" src=<?= "http://localhost:8080" . $imageUrl ?> />
             <div class="d-flex justify-content-between mt-3">
