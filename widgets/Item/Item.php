e3fff271ddf54fa50680c0cbedcdf823107f9f2e
matousek11 <matousek1199@gmail.com>
2023-08-27 17:02:51 +0200

    item widget added

--- widgets/Item/Item.php
+++ widgets/Item/Item.php
@@ -1,5 +1,31 @@
 <?php
 
-namespace app\widgets;
+namespace app\widgets\Item;
 
+use app\widgets\Item\assets\ItemAsset;
 use yii\base\Widget;
+
+class Item extends Widget
+{
+    public int $id;
+    public string $productName;
+    public int $price;
+    public string $imageUrl;
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        ItemAsset::register($this->getView());
+
+        return $this->render("item-view", [
+            "id" => $this->id,
+            "productName" => $this->productName,
+            "price" => $this->price,
+            "imageUrl" => $this->imageUrl
+        ]);
+    }
+}
---
+++ widgets/Item/assets/ItemAsset.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\widgets\Item\assets;
+
+use yii\web\AssetBundle;
+
+class ItemAsset extends AssetBundle
+{
+    public $sourcePath = "@app/widgets/Item/assets";
+    public $css = ["css/item.css"];
+}
